5f8d233b7609b68633b123f7457195a224ca18fe
jkap24 <amealoplan@gmail.com>
2023-11-28 16:27:27 +0000

    centralise navbar contents

--- src/components/AmealoNavBar/AmealoNavBar.tsx
+++ src/components/AmealoNavBar/AmealoNavBar.tsx
@@ -5,7 +5,7 @@ function AmealoNavBar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary position-static">
-                <div className="container-fluid">
+                <div className="container">
                     <a className="navbar-brand" href="/home">Amealo</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
